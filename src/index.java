280e29ef3a34a5f6fc1a7377d0ebbc93da5b20e4
Sydney Kuhn <sydney.kuhn@mths.ca>
2022-09-15 13:35:21 +0000

    added log calculator

---
+++ src/index.java
@@ -0,0 +1,49 @@
+/*
+ * The log counter calculates the maximum amount of logs
+ * a truck can carry based on the length of the logs.
+ *
+ * @author  Sydney Kuhn
+ * @version 1.0
+ * @since   2022-09-14
+ */
+
+import java.io.Console;
+
+/**
+* This is the standard "Hello, World!" program.
+*/
+final class logCalculator {
+
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this ever is called
+    *
+    * @throws IllegalStateException
+    *
+    */
+    private logCalculator() {
+	throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(final String[] args) {
+	final Console console = System.console();
+        final String logLengthString = console.readLine(
+		"How long are the logs in meters : ");
+
+        final float logLength = Float.valueOf(logLengthString);
+
+        final float logAmount = 1100 / (20 * logLength);
+
+	// print out answer
+	System.out.println(
+		logAmount + " is the maximum amount of " + logLength + "m logs.");
+        System.out.println("\nDone.");
+    }
+}
+
